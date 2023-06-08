9bc9ecd235c14d00630db415f4e4135f426858bd
ragranato <robert.granato@gmail.com>
2023-06-08 17:20:49 -0400

    working on rendering cards

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,12 +7,13 @@ let routine = document.getElementById('routine')
 let due = document.getElementById('due');
 const submit = document.getElementById('submit')
 let urgency = urgent.checked ? urgent : routine;
-
+let taskList = document.getElementById('task-list');
 
 
 function handleSubmit(){
     event.preventDefault()
     taskmanager.addTask(task.value, description.value, category.value, assign.value, urgency.value, due.value)
+    taskmanager.render()
     console.log(taskmanager.tasks)
 }
 
