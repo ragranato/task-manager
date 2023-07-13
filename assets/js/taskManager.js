68807707a5488db940d49afb64e1ff156419719b
ragranato <robert.granato@gmail.com>
2023-07-12 22:52:25 -0400

    completed tasks 8 and 9. Added delete capability and local storage

--- assets/js/taskManager.js
+++ assets/js/taskManager.js
@@ -3,8 +3,9 @@ let taskList = document.getElementById("task-list");
 const createTaskHtml = (object) => {
   const html = `                 
             <div class="card-body pb-5" id=${object.id}>
-                <div class="d-flex justify-content-end">
-                  <button type="button" class="btn btn-primary mb-2 done-button">${object.status}</button>
+                <div class="d-flex  card-btns">
+                  <button type="button" class="btn btn-primary mb-2 done-button">${object.status}</button>   
+                  <button type="button" class="btn btn-primary mb-2 delete-button">Delete</button>               
                 </div>                
                 <h5 class="card-title">${object.task}</h5>
                 <h6 class="card-subtitle mb-2 text-danger">${object.urgency}</h6>
@@ -26,7 +27,7 @@ class TaskManager {
     this.currentId = 0;
   }
 
-  addTask(task, description, category, assign, urgency, due, status = "TODO") {
+  addTask(task, description, category, assign, urgency, due, status = "Pending") {
     this.currentId++;
     const taskObj = {
       id: this.currentId,
@@ -39,6 +40,34 @@ class TaskManager {
       status,
     };
     this.tasks.push(taskObj);   
+  }
+
+  save() {
+    const taskJSON = JSON.stringify(this.tasks)
+    const currentIdString = String(this.currentId)
+    localStorage.setItem("tasks", taskJSON)
+    localStorage.setItem("currentId", currentIdString)
+  }
+
+  load() {
+    if (localStorage.getItem('tasks') !== null) {
+      const tasksJSON = localStorage.getItem('tasks')
+      this.tasks = JSON.parse(tasksJSON)
+    }
+    if (localStorage.getItem('currentId') !== null) {
+      const currentId = Number(localStorage.getItem('currentId'))
+      this.currentId = currentId
+    }
+  }
+
+  deleteTask(taskId) {
+    const newTasks = []
+    for (let i = 0; i < this.tasks.length; i++) {
+      if (taskId !== this.tasks[i]['id']) {
+        newTasks.push(this.tasks[i])
+      }
+    }
+    this.tasks = newTasks
   }
 
   render() {
